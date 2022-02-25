8a20555b7b0660a29f66e048e8fb95cdd5c2ec88
Jose Lujan <jose.lujan@inei.gob.pe>
2022-02-24 22:41:08 -0500

    app demo
    app demo voice

--- app/src/main/java/com/example/speechtotext_lujan/MainActivity.java
+++ app/src/main/java/com/example/speechtotext_lujan/MainActivity.java
@@ -1,14 +1,136 @@
 package com.example.speechtotext_lujan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
+    EditText editText;
+    int count=0;
 
+    SpeechRecognizer speechRecognizer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton=findViewById(R.id.button);
+        editText=findViewById(R.id.edittext);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},1);
+        }
+
+        speechRecognizer=SpeechRecognizer.createSpeechRecognizer(this);
+
+        final Intent speechReconizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(count==0)
+                {
+                    imageButton.setImageDrawable(getDrawable(R.drawable.ic_baseline_mic_24));
+
+                    //startlstening
+
+                    speechRecognizer.startListening(speechRecognizer);
+                    count=1;
+                }
+                else
+                {
+                    imageButton.setImageDrawable(getDrawable(R.drawable.ic_baseline_mic_off_24));
+
+                    //stop listening
+                    speechRecognizer.stopListening();
+                    count=0;
+                }
+
+            }
+        });
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> data= results.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
+
+                editText.setText(data.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1)
+        {
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this,"Pemission Granted" ,Toast.LENGTH_SHORT);
+            }
+            else
+            {
+                Toast.makeText(this,"Pemission Denied" ,Toast.LENGTH_SHORT);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
